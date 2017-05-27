77d2217b0ccaa18f1080d8c90717d364c385aec8
Michael Prather <mprather1@gmail.com>
2017-05-27 06:56:05 +0000

    update

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const dir = process.argv[2]
 const moment = require('moment')
 
 fs.readdir(dir, (err, files) => {
+  if (err) {
+    console.log(err)
+  }
+
   for (var i = 0; i < files.length; i++) {
-    const date = Date()
-    fs.rename(dir + files[i], dir + moment().format('YYYY-MM-DD') + '-' + files[i] + '-' + (i+1))
+    fs.rename(dir + files[i], dir + moment().format('YYYY-MM-DD') + '-' + files[i] + '-' + (i + 1))
   }
 })
