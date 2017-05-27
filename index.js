fa42beb00a83df5b7439168ba642feeba8a49fd3
Michael Prather <mprather1@gmail.com>
2017-05-27 07:14:36 +0000

    ask for confirmation

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const fs = require('fs')
-const dir = process.argv[2]
+const dirname = process.argv[2]
 const moment = require('moment')
 
-fs.readdir(dir, (err, files) => {
-  if (err) {
-    console.log(err)
-  }
+console.log(`Are you sure you want to rename the files in ${dirname}? y/n`)
 
-  for (var i = 0; i < files.length; i++) {
-    fs.rename(dir + files[i], dir + moment().format('YYYY-MM-DD') + '-' + files[i] + '-' + (i + 1))
+process.stdin.setRawMode(true)
+
+process.stdin.once('data', (data) => {
+  data = data.toString()
+  console.log(data.toString())
+  if (data === 'y' || data === 'yes') {
+    renameFiles(dirname)
+  } else {
+    process.exit()
   }
 })
+
+function renameFiles (dir) {
+  fs.readdir(dir, (err, files) => {
+    if (err) {
+      console.log(err)
+    }
+
+    for (var i = 0; i < files.length; i++) {
+      fs.rename(dir + files[i], dir + moment().format('YYYY-MM-DD') + '-' + files[i] + '-' + (i + 1))
+    }
+    process.exit()
+  })
+}
